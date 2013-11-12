fe9dc5e23d652ec2c3f915dc321a17f469ce504f
Charlie Root <root@sv100.bhaoo.com>
2013-11-12 09:19:34 +0000

    updated wrapper and made available put file and get file function

--- openssh_wrapper.py
+++ openssh_wrapper.py
@@ -265,6 +265,116 @@ class SSHConnection(object):
                     raise SSHError("change owner: %s" % result.stderr.strip())
         cleanup_tmp_dir()
 
+    def put(self, filename, target, mode=None, owner=None):
+        """ Copy file identified by their name to remote location
+
+        :param filename: file name to copy..
+
+        :param target: target file or directory to copy data to. Target file
+        makes sense only if the number of files to copy equals to one.
+
+        :param mode: optional parameter to define mode for every uploaded file
+        (must be a string in the form understandable by chmod, like "0644")
+
+        :param owner: optional parameter to define user and group for every
+        uploaded file (must be a string in the form understandable by chown).
+        Makes sense only if you open your connection as root.
+
+        :return: None
+        :raise: SSHError
+        """
+        scp_command = self.scp_command_put(filename, target)
+        pipe = subprocess.Popen(scp_command,
+                stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE, env=self.get_env())
+        try:
+            signal.signal(signal.SIGALRM, _timeout_handler)
+        except ValueError:  # signal only works in main thread
+            pass
+        signal.alarm(self.timeout)
+        err = b('')
+        try:
+            _, err = pipe.communicate()
+        except IOError as exc:
+            # pipe.terminate() # only in python 2.6 allowed
+            os.kill(pipe.pid, signal.SIGTERM)
+            signal.alarm(0)  # disable alarm
+            raise SSHError(str(exc))
+        signal.alarm(0)  # disable alarm
+        returncode = pipe.returncode
+        if returncode != 0:  # ssh client error
+            raise SSHError(err.strip())
+
+        if mode or owner:
+            targets = self.get_scp_targets(filename, target)
+            if mode:
+                cmd_chunks = ['chmod', mode] + targets
+                cmd = b_quote(cmd_chunks)
+                result = self.run(cmd)
+                if result.returncode:
+                    raise SSHError(result.stderr.strip())
+            if owner:
+                cmd_chunks = ['chown', owner] + targets
+                cmd = b_quote(cmd_chunks)
+                result = self.run(cmd)
+                if result.returncode:
+                    raise SSHError(result.stderr.strip())
+
+    def get(self, filename, target, mode=None, owner=None):
+        """ Copy file identified by their name from remote location
+
+        :param filename: file name to copy..
+
+        :param target: target file or directory to copy data to. Target file
+        makes sense only if the number of files to copy equals to one.
+
+        :param mode: optional parameter to define mode for every uploaded file
+        (must be a string in the form understandable by chmod, like "0644")
+
+        :param owner: optional parameter to define user and group for every
+        uploaded file (must be a string in the form understandable by chown).
+        Makes sense only if you open your connection as root.
+
+        :return: None
+        :raise: SSHError
+        """
+        scp_command = self.scp_command_get(filename, target)
+        pipe = subprocess.Popen(scp_command,
+                stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE, env=self.get_env())
+        try:
+            signal.signal(signal.SIGALRM, _timeout_handler)
+        except ValueError:  # signal only works in main thread
+            pass
+        signal.alarm(self.timeout)
+        err = b('')
+        try:
+            _, err = pipe.communicate()
+        except IOError as exc:
+            # pipe.terminate() # only in python 2.6 allowed
+            os.kill(pipe.pid, signal.SIGTERM)
+            signal.alarm(0)  # disable alarm
+            raise SSHError(str(exc))
+        signal.alarm(0)  # disable alarm
+        returncode = pipe.returncode
+        if returncode != 0:  # ssh client error
+            raise SSHError(err.strip())
+
+        if mode or owner:
+            targets = self.get_scp_targets(filename, target)
+            if mode:
+                cmd_chunks = ['chmod', mode] + targets
+                cmd = b_quote(cmd_chunks)
+                result = self.run(cmd)
+                if result.returncode:
+                    raise SSHError(result.stderr.strip())
+            if owner:
+                cmd_chunks = ['chown', owner] + targets
+                cmd = b_quote(cmd_chunks)
+                result = self.run(cmd)
+                if result.returncode:
+                    raise SSHError(result.stderr.strip())
+
     def convert_files_to_filenames(self, files):
         """
         Helper function which is invoked by scp.
@@ -378,6 +488,50 @@ class SSHConnection(object):
 
         cmd += files
         cmd.append('%s:%s' % (remotename, target))
+        return b_list(cmd)
+
+    def scp_command_put(self, filename, target):
+        """
+        Build the command string to transfer the files identified by filenames.
+
+        Include target(s) if specified. Internal function
+        """
+        cmd = ['/usr/bin/scp', '-q', '-r']
+        if self.login:
+            remotename = '%s@%s' % (u(self.login), u(self.server))
+        else:
+            remotename = self.server
+        if self.configfile:
+            cmd += ['-F', self.configfile]
+        if self.identity_file:
+            cmd += ['-i', self.identity_file]
+        if self.port:
+            cmd += ['-P', self.port]
+
+        cmd.append(filename)
+        cmd.append('%s:%s' % (remotename, target))
+        return b_list(cmd)
+
+    def scp_command_get(self, filename, target):
+        """
+        Build the command string to transfer the files identified by filenames.
+
+        Include target(s) if specified. Internal function
+        """
+        cmd = ['/usr/bin/scp', '-q', '-r']
+        if self.login:
+            remotename = '%s@%s' % (u(self.login), u(self.server))
+        else:
+            remotename = self.server
+        if self.configfile:
+            cmd += ['-F', self.configfile]
+        if self.identity_file:
+            cmd += ['-i', self.identity_file]
+        if self.port:
+            cmd += ['-P', self.port]
+
+        cmd.append('%s:%s' % (remotename, filename))
+        cmd.append(target)
         return b_list(cmd)
 
     def get_env(self):
